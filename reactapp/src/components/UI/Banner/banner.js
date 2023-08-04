4b80100d80a1359c5f986b50204e36e915ee6830
Iamneo <techadmin@iamneo.ai>
2023-08-04 04:18:06 +0000

    Backup Commit

--- reactapp/src/components/UI/Banner/banner.js
+++
@@ -1,8 +0,0 @@
-import {useState} from "react";
-const Banner = ({correctAnswered})=>{
-    const [ questionsCorrect ] = useState(correctAnswered);
-return (
-    <h3>You have answered {questionsCorrect} / 5  Correctly</h3> 
-)
-}
-export default Banner;
